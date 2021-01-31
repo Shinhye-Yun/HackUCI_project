b90849f889c6e6b6aba9b86d4a229bea7f00a3e9
yun-sh <78223029+yun-sh@users.noreply.github.com>
2021-01-31 01:55:49 -0500

    updated app2.js
    fixed typo and deleted unused functions

--- app2.js
+++ app2.js
@@ -1,41 +1,17 @@
 var name;
-var sirname;
+var surname;
 var dob;
 var pet;
 var email
 var phone;
 
-function getName() {
-    event.preventDefault();
-    name = document.getElementById("name").value;
-    console.log(name);
-}
-
-function getEmail() {
-    event.preventDefault();
-    email = document.getElementById("email").value;
-    console.log(email);
-}
-
-function getPhone() {
-    event.preventDefault();
-    phone = document.getElementById("phone").value;
-    console.log(phone);
-}
-
-function getSns() {
-    event.preventDefault();
-    sns = document.getElementById("sns").value;
-    console.log(sns);
-}
-
 function getInfo() {
     name = document.getElementById("name").value;
-    sirname = document.getElementById("sirname").value;
+    surname = document.getElementById("surname").value;
     dob = document.getElementById("dob").value;
     pet = document.getElementById("pet").value;
     email = document.getElementById("email").value;
     phone = document.getElementById("phone").value;
 
-    console.log(name + " " + sirname + " " + dob + " " + pet + " " + email + " " + phone);
+    console.log(name + " " + surname + " " + dob + " " + pet + " " + email + " " + phone);
 }
